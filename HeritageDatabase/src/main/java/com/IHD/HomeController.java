1eae0e0f97fb00cab992ecce128b04ecf9608006
Abhinav Singh <abhinavsingh26@gmail.com>
2022-02-22 13:49:20 +0530

    updated

--- HeritageDatabase/src/main/java/com/IHD/HomeController.java
+++ HeritageDatabase/src/main/java/com/IHD/HomeController.java
@@ -2,13 +2,16 @@ package com.IHD;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 	
 	@RequestMapping("home")
-	public void home() {
+	@ResponseBody
+	public String home() {
 		System.out.println("hi");
+		return "home.html";
 	}
 	
 }
