d02b22533a2277321b2ecaa49bcb85233df79e5b
Abhinav Singh <abhinavsingh26@gmail.com>
2022-02-22 12:00:16 +0530

    Initial commit

---
+++ HeritageDatabase/src/main/java/com/IHD/HeritageDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.IHD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeritageDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeritageDatabaseApplication.class, args);
+	}
+
+}
---
+++ HeritageDatabase/src/test/java/com/IHD/HeritageDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IHD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeritageDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
